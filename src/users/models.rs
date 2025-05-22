18cb0fee75b240039ca3a7363ee6fb245ab69f34
Pablo Suárez <104940521+psuarezdev@users.noreply.github.com>
2025-05-22 13:46:41 +0100

    Update models.rs

--- src/users/models.rs
+++ src/users/models.rs
@@ -2,6 +2,7 @@ use crate::schema::users;
 use diesel::prelude::*;
 use serde::{Deserialize, Serialize};
 use chrono::NaiveDateTime;
+use validator::Validate;
 
 #[derive(Debug, Queryable, Insertable, Serialize, Deserialize)]
 #[diesel(table_name = users)]
@@ -16,11 +17,15 @@ pub struct User {
     pub updated_at: NaiveDateTime,
 }
 
-#[derive(Insertable, Deserialize)]
+#[derive(Insertable, Deserialize, Validate)]
 #[diesel(table_name = users)]
 pub struct NewUser {
+    #[validate(length(min = 1, message = "First name is required"))]
     pub first_name: String,
+    #[validate(length(min = 1, message = "Last name is required"))]
     pub last_name: String,
+    #[validate(email(message = "Email must be a valid email address"))]
     pub email: String,
+    #[validate(length(min = 8, message = "Password must be at least 8 characters long"))]
     pub password: String,
 }
